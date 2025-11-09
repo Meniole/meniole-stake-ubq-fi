11e2cf40c6c534671b85687582bdfe42425feff1
codingsh <codingsh@pm.me>
2025-11-09 18:21:48 +0400

    chore: update button component to accept asyc function to reown connect

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,18 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> {
   isLoading?: boolean;
   isLoadingText?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void | Promise<void | { hash: string; } | undefined>;
 }
 
-export function Button({ children, disabled, isLoading, isLoadingText, ...props }: ButtonProps) {
+export function Button({ children, disabled, isLoading, isLoadingText, onClick, ...props }: ButtonProps) {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
   return (
-    <button {...props} disabled={isLoading || disabled}>
+    <button {...props} onClick={handleClick} disabled={isLoading || disabled}>
       {isLoading ? (
         <>
           <div className="spinner button-spinner"></div>
